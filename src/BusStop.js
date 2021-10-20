85cd0c42837e9cfbc263847999cc57b6f42d870e
toreo23 <4fjacktam@gmail.com>
2021-10-21 02:18:33 +0800

    more friendly for mobile

--- src/BusStop.js
+++ src/BusStop.js
@@ -5,6 +5,7 @@ import React, {
   useImperativeHandle
 } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Grid from "@material-ui/core/Grid";
@@ -18,6 +19,8 @@ import moment from "moment";
 import Autocomplete from "./Autocomplete";
 
 import kmbwater from "./img/kmb-water.png";
+
+import { isMobile } from 'react-device-detect';
 
 const useStyles = makeStyles((theme) => ({
   // root: {
@@ -37,6 +40,9 @@ const useStyles = makeStyles((theme) => ({
     align: "center"
   }
 }));
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 const BusStop = (props, ref) => {
   const classes = useStyles();
@@ -75,11 +81,14 @@ const BusStop = (props, ref) => {
       });
   };
 
-  const parseETA = (eta) => {
+  const renderETA = (eta) => {
     var exp = moment(eta);
     var etaInSecond = moment().diff(exp, "seconds") * -1;
     var etaInMin = moment().diff(exp, "minutes") * -1;
-    return etaInSecond > 60 ? etaInMin + " min" : "- min";
+    if(eta == null) return <Typography variant={isMobile?"subtitle2":"h5"}>沒有班次</Typography>
+    return <Typography variant={isMobile?"h6":"h4"}>{etaInSecond > 60 ? etaInMin + " min" : "- min"}</Typography>
+
+    
   };
 
   const handleIncrement = () => {
@@ -108,105 +117,111 @@ const BusStop = (props, ref) => {
   );
 
   return (
-    <Grid>
-      <Grid container style={{ padding: "5px" }}>
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
-        >
-          <Grid align="center" style={{ flex: 2 }}>
-            <Autocomplete
-              changeSelectStop={changeSelectStop}
-              defaultStop={props.defaultStop}
-            />
-          </Grid>
-        </Grid>
-        <Grid style={{ flex: 1 }} alignItems="center" justifyContent="center">
-          <ButtonGroup>
-            <Button onClick={handleIncrement}>+</Button>
-            {displayCounter && <Button disabled>{numETA}</Button>}
-            {displayCounter && <Button onClick={handleDecrement}>-</Button>}
-          </ButtonGroup>
-        </Grid>
-        <Grid style={{ flex: 1, align: "center" }}>
-          <Typography align="right">
-            Last Update Time: {moment(lastupdts).format("YYYY/MM/DD HH:mm:ss")}
-          </Typography>
-          <Typography align="right">
-            Last Refresh Time:{" "}
-            {moment(lastrefreshts).format("YYYY/MM/DD HH:mm:ss")}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid direction="row" justify="space-between" alignItems="center">
-        {station.map((route, index) => (
+    <ThemeProvider theme={theme}>
+      <Grid>
+        <Grid container style={{ padding: "5px", minWidth: isMobile?"60vw":"40vw" }}>
+        
           <Grid
             container
             direction="row"
             justify="space-between"
             alignItems="center"
           >
-            <Grid
-              style={{
-                flex: 1
-                //backgroundColor: "red"
-              }}
-            >
-              {route.eta_seq === 1 && (
-                <Typography align="center" variant="h3">
-                  {route.route}
-                </Typography>
-              )}
+            <Grid align="center" style={{ flex: 2 }}>
+              <Autocomplete
+                changeSelectStop={changeSelectStop}
+                defaultStop={props.defaultStop}
+              />
             </Grid>
-            <Grid
-              style={{
-                flex: 4
-                //backgroundColor: "red"
-              }}
-            >
-              {route.eta_seq === 1 && (
-                <Typography
-                  align="center"
-                  variant="h5"
-                  className={classes.typography}
-                >
-                  <Typography variant="h5" className={classes.typography}>
-                    {route.dest_tc}
-                  </Typography>
-                  <Typography variant="h6" className={classes.typography}>
-                    {route.dest_en}
-                  </Typography>
-                </Typography>
-              )}
-            </Grid>
-            {/* <ListItemText primary={route.route} secondary={route.dest_tc} /> */}
-            <Grid
-              style={{
-                flex: 1
-                //backgroundColor: "red"
-              }}
-            >
-              <Typography align="center" className={classes.min}>
-                {/* <Typography>{moment(route.eta).fromNow()}</Typography>
-              <Typography>{route.rmk_tc}</Typography> */}
-                <Typography variant="h4">{parseETA(route.eta)}</Typography>
-                <Typography variant="h6">{route.rmk_tc}</Typography>
-              </Typography>
-            </Grid>
-            <img
-              src={kmbwater}
-              className="photo"
-              alt="KMB"
-              width="100px"
-              height="100px"
-            />
           </Grid>
-        ))}
+          <Grid alignItems="left" justifyContent="center">
+            <ButtonGroup>
+              <Button onClick={handleIncrement}>+</Button>
+              {displayCounter && <Button disabled>{numETA}</Button>}
+              {displayCounter && <Button onClick={handleDecrement}>-</Button>}
+            </ButtonGroup>
+          </Grid>
+          <Grid direction="row" style={{ flex: 1, align: "center" }}>
+            <Typography align="right" variant="subtitle2">
+              Last Update: {moment(lastupdts).format("YYYY/MM/DD HH:mm:ss")}
+            </Typography>
+            <Typography align="right" variant="subtitle2">
+              Last Refresh:{" "}
+              {moment(lastrefreshts).format("YYYY/MM/DD HH:mm:ss")}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid direction="row" justify="space-between" alignItems="center">
+          {station.map((route, index) => (
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+              style={{
+                minHeight: '10vh'
+              }}
+            >
+              <Grid
+                style={{
+                  flex: 1,
+                }}
+              >
+                {route.eta_seq === 1 && (
+                  <Typography align="center" variant="h3">
+                    {route.route}
+                  </Typography>
+                )}
+              </Grid>
+              <Grid
+                style={{
+                  flex: 4
+                  //backgroundColor: "red"
+                }}
+              >
+                {route.eta_seq === 1 && (
+                  <Typography
+                    align="center"
+                    variant="h5"
+                    className={classes.typography}
+                  >
+                    <Typography variant="h5" className={classes.typography}>
+                      {route.dest_tc}
+                    </Typography>
+                    {!isMobile &&
+                    <Typography variant="h6" className={classes.typography}>
+                      {route.dest_en}
+                    </Typography>}
+                  </Typography>
+                )}
+              </Grid>
+              {/* <ListItemText primary={route.route} secondary={route.dest_tc} /> */}
+              <Grid
+                style={{
+                  flex: 1
+                  //backgroundColor: "red"
+                }}
+              >
+                <Typography align="center" className={classes.min}>
+                  {/* <Typography>{moment(route.eta).fromNow()}</Typography>
+              <Typography>{route.rmk_tc}</Typography> */}
+
+                  {renderETA(route.eta)}
+                  <Typography variant={!isMobile?"h6":"subtitle2"}>{route.rmk_tc}</Typography>
+                </Typography>
+              </Grid>
+              {!isMobile && <img
+                src={kmbwater}
+                className="photo"
+                alt="KMB"
+                width="100px"
+                height="100px"
+              />}
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
-    </Grid>
-  );
+    </ThemeProvider>);
 };
 
 export default forwardRef(BusStop);
